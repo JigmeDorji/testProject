3c31df69615a363a6c383c512702ff4f547f1a96
jimmychoxiling <jeecee191@gmail.com>
2020-11-08 17:35:39 +0600

    gewog table

---
+++ tbl_gewog.sql
@@ -0,0 +1,36 @@
+/*
+SQLyog Ultimate v8.82 
+MySQL - 5.5.5-10.4.14-MariaDB : Database - training_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`training_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `training_db`;
+
+/*Table structure for table `tbl_gewog` */
+
+DROP TABLE IF EXISTS `tbl_gewog`;
+
+CREATE TABLE `tbl_gewog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gewogName` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `tbl_gewog` */
+
+insert  into `tbl_gewog`(`id`,`gewogName`) values (1,'Deothang'),(2,'Orong');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
