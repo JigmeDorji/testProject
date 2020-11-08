700048417283f8a115bfc919a1ca672c6fecff7a
Jigme Dorji <dorjijigme32@gmail.com>
2020-11-08 16:53:24 +0600

    chiwog has created

--- src/main/webapp/resources/lib/customLib/script-loader.js
+++ src/main/webapp/resources/lib/customLib/script-loader.js
@@ -2,10 +2,13 @@
 
     if (document.URL.search("studentRegistration") > 1)
         scriptLoader("resources/js/studentRegistration.js");
+    if (document.URL.search("chiwogRegistration") > 1)
+        scriptLoader("resources/js/chiwogRegistration.js");
 
 });
 
 var scriptLoader = function (url) {
+
     $.ajax(
         {
             type: "GET",
