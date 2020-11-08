c05f22e9e8d04c1be9a3c8d85876548b6c2f6e05
nzepa74 <nzepa.ttpl@gmail.com>
2020-11-08 16:53:48 +0600

    done country Setup

---
+++ src/main/java/com/test/srv/lis/dto/countrySetupDTO.java
@@ -0,0 +1,13 @@
+package com.test.srv.lis.dto;
+
+public class countrySetupDTO {
+    private String countryName;
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+}
--- src/main/webapp/resources/lib/customLib/script-loader.js
+++ src/main/webapp/resources/lib/customLib/script-loader.js
@@ -3,6 +3,9 @@
     if (document.URL.search("studentRegistration") > 1)
         scriptLoader("resources/js/studentRegistration.js");
 
+    if (document.URL.search("countrySetup") > 1)
+        scriptLoader("resources/js/countrySetup.js");
+
 });
 
 var scriptLoader = function (url) {
