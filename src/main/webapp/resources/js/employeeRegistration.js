3a39fc7ffd71b8886f1788ffb16eff41e3e499c2
passangwaiba <dagappasa@gmail.com>
2020-11-10 09:57:18 +0600

    updates

--- src/main/webapp/resources/js/employeeRegistration.js
+++ src/main/webapp/resources/js/employeeRegistration.js
@@ -10,51 +10,6 @@ employeeRegistration = (function () {
         return 'employeeRegistration/';
     }
 
-    function savedd() {
-        $('#btnSave').on('click', function () {
-            $.validator.setDefaults({
-                submitHandler: function () {
-                    $('#btnSave').attr('disabled', true);
-                    //var data = $('#studentRegistrationForm').serializeArray();
-                    var data = new FormData($('.employeeRegistrationForm')[0]);
-                    var url = _baseURL() + 'save';
-                    $.ajax({
-                        url: url,
-                        type: 'post',
-                        data: data,
-                        processData: false,
-                        success: function (res) {
-                            if (res.status == 1) {
-                                successMsg(res.text);
-                                $('#employeeModal').modal('hide');
-                                $('#btnSave').attr("disabled", false);
-                            } else {
-                                $('#btnSave').attr("disabled", false);
-                                warningMsg(res.text);
-                            }
-                        }, complete: function (res) {
-                            $('#btnSave').attr("disabled", false);
-                            warningMsg(res.text);
-                        }
-                    });
-                }
-            });
-            form.validate({
-                errorElement: 'span',
-                errorPlacement: function (error, element) {
-                    error.addClass('invalid-feedback');
-                    element.closest('.col-4').append(error);
-                },
-                highlight: function (element, errorClass, validClass) {
-                    $(element).addClass('is-invalid');
-                },
-                unhighlight: function (element, errorClass, validClass) {
-                    $(element).removeClass('is-invalid');
-                }
-            });
-        });
-    }
-
     function save() {
         $('.employeeRegistrationForm').validate({
             submitHandler: function (form) {
@@ -67,6 +22,8 @@ employeeRegistration = (function () {
                     success: function (res) {
                         if (res.status == 1) {
                             successMsg(res.text);
+                            $('#employeeModal').modal('hide');
+                            $('#btnSave').attr("disabled", false);
 
                             getEmployeeList();
                         }
@@ -135,6 +92,7 @@ employeeRegistration = (function () {
                     success: function (res) {
                         if (res.status == 1) {
                             successMsg(res.text);
+
                             getEmployeeList();
                         } else {
                             errorMsg(res.text)
