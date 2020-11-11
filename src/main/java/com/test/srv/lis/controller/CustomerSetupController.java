08fd2ee95c56f58d5656e42798caaa65c2739dbd
Tenzin11mo <tenishlhazim@gmail.com>
2020-11-11 12:12:06 +0600

    hi

--- src/main/java/com/test/srv/lis/controller/CustomerSetupController.java
+++ src/main/java/com/test/srv/lis/controller/CustomerSetupController.java
@@ -1,9 +1,19 @@
 package com.test.srv.lis.controller;
 
+import com.test.srv.helper.CurrentUser;
+import com.test.srv.helper.ResponseMessage;
+import com.test.srv.lis.dto.CustomerSetupDTO;
+import com.test.srv.lis.service.CustomerSetupService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Description: CustomerSetup
@@ -21,13 +31,31 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 @PreAuthorize("isAuthenticated()")
-@RequestMapping("customerSetup")
+@RequestMapping("/customerSetup")
 public class CustomerSetupController {
+    @Autowired
+    private CustomerSetupService customerSetupService;
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index() {
         return "customerSetup";
     }
 
+
+    @ResponseBody
+    @RequestMapping(value = "/getCustomerSetup", method = RequestMethod.GET)
+    public List<CustomerSetupDTO> getCustomerSetup() throws IOException {
+        return customerSetupService.getCustomerSetup();
+    }
+    @ResponseBody
+    @RequestMapping(value = "/saveCustomerSetup", method = RequestMethod.POST)
+    public ResponseMessage saveCustomerSetup(CustomerSetupDTO customerSetupDTO) throws IOException {
+        return customerSetupService.saveCustomerSetup(customerSetupDTO);
+    }
+    @ResponseBody
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public ResponseMessage delete(Integer id) throws IOException {
+        return customerSetupService.delete(id);
+    }
 
 }
