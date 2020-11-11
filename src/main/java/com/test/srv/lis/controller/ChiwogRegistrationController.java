3dcd5139c55443a8d3593c44e35b636bf8a8f6aa
karma <karmatshenda7@gmail.com>
2020-11-11 13:15:21 +0600

    success

--- src/main/java/com/test/srv/lis/controller/ChiwogRegistrationController.java
+++ src/main/java/com/test/srv/lis/controller/ChiwogRegistrationController.java
@@ -1,10 +1,16 @@
 package com.test.srv.lis.controller;
 
+import com.test.srv.enumeration.SystemDataInt;
+import com.test.srv.helper.BaseController;
 import com.test.srv.helper.CurrentUser;
 import com.test.srv.helper.ResponseMessage;
+import com.test.srv.integration.jasper.report.domain.dto.ReportResponseDto;
 import com.test.srv.lis.dto.ChiwogRegistrationDTO;
 import com.test.srv.lis.dto.StudentRegDTO;
+import com.test.srv.lis.entity.StudentRegistration;
 import com.test.srv.lis.service.ChiwogRegistrationService;
+import com.test.srv.lis.service.StudentRegistrationService;
+import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -15,7 +21,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by jigme.dorji on 11/8/2020.
@@ -23,11 +33,10 @@ import java.util.List;
 @Controller
 @PreAuthorize("isAuthenticated()")
 @RequestMapping("/chiwogRegistration")
-public class ChiwogRegistrationController {
+public class ChiwogRegistrationController extends BaseController {
 
     @Autowired
     ChiwogRegistrationService chiwogRegistrationService;
-
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String list(HttpServletRequest request, Model model) {
@@ -54,5 +63,32 @@ public class ChiwogRegistrationController {
         return chiwogRegistrationService.deleteChiwogInfoByChiwogId(chiwogId);
 
     }
+    @ResponseBody
+    @RequestMapping(value = "/generateReport", method = {RequestMethod.GET})
+    public ResponseMessage generateReport(HttpServletRequest request, Integer companyId, Integer financialYearId, String fileFormat, Integer compactTypeId) throws JRException, SQLException, ClassNotFoundException {
+        CurrentUser currentUser = (CurrentUser) request.getSession().getAttribute("currentUser");
+
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("reportTitle", "Chiwog Registration System");
+        params.put("reportSubTitle", "Chiwog Negotiation Meeting");
+
+        String reportSourcePath = getReportSourcePath(request);
+        String reportOutputPath = getReportOutputPath(request);
+        if (fileFormat == null || Objects.equals(fileFormat, "") || fileFormat.equalsIgnoreCase("Preview")) {
+            fileFormat = "pdf";
+        }
+        ResponseMessage responseMessage = new ResponseMessage();
+        try {
+            ReportResponseDto reportResponseDto = chiwogRegistrationService.generateReport(fileFormat, params,
+                    reportSourcePath, reportOutputPath, 1, getCurrentUser(request).getLoginId(), compactTypeId);
+            responseMessage.setStatus(SystemDataInt.MESSAGE_STATUS_SUCCESSFUL.value());
+            responseMessage.setDTO(reportResponseDto);
+        } catch (Exception ex) {
+            responseMessage.setStatus(SystemDataInt.MESSAGE_STATUS_UNSUCCESSFUL.value());
+            responseMessage.setText("Report generation failed. " + ex);
+        }
+        return responseMessage;
+    }
 
 }
+
--- src/main/java/com/test/srv/lis/service/ChiwogRegistrationService.java
+++ src/main/java/com/test/srv/lis/service/ChiwogRegistrationService.java
@@ -4,17 +4,28 @@ package com.test.srv.lis.service;
  * Created by jigme.dorji on 11/8/2020.
  */
 
+import com.test.srv.helper.BaseService;
 import com.test.srv.helper.ResponseMessage;
+import com.test.srv.integration.jasper.report.domain.dto.ReportRequestDto;
+import com.test.srv.integration.jasper.report.domain.dto.ReportResponseDto;
+import com.test.srv.integration.jasper.report.service.ReportService;
 import com.test.srv.lis.dao.ChiwogRegistrationDao;
 import com.test.srv.lis.dto.ChiwogRegistrationDTO;
 import com.test.srv.lis.entity.ChiwogRegistration;
+import net.sf.jasperreports.engine.JRException;
+import org.hibernate.internal.SessionImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.List;
+import java.util.Map;
 
 @Service
-public class ChiwogRegistrationService {
+public class ChiwogRegistrationService extends BaseService {
 
     @Autowired
     ChiwogRegistrationDao chiwogRegistrationDao;
@@ -50,5 +61,30 @@ public class ChiwogRegistrationService {
         responseMessage.setStatus(1);
         responseMessage.setText("chiwog information succefully deleted");
         return responseMessage;
+    }
+
+    @Transactional
+    public ReportResponseDto generateReport(String reportType, Map<String, Object> params, String reportPath,
+                                            String outputPath, Integer reportSelection, String userID, Integer compactTypeId) throws
+            JRException, ClassNotFoundException, SQLException, ParseException {
+
+        Connection connection = ((SessionImpl) getCurrentSession()).connection();
+
+        String headerName = "";
+        String footerName = "";
+
+        reportPath = reportPath.replace("\\", "/");
+
+        String reportJRXML  = "/compact.documents/chiwogRegistrationReport.jrxml";
+        String reportName = "studentListReport";
+
+        headerName = ReportService.portraitHeaderName;
+        footerName = ReportService.portraitFooterName;
+
+        ReportRequestDto reportRequestDto = new ReportRequestDto(outputPath, reportName, reportType,
+                reportPath + headerName, reportPath + reportJRXML, reportPath + footerName,
+                reportPath, params, connection, userID);
+
+        return ReportService.createReport(reportRequestDto);
     }
 }
